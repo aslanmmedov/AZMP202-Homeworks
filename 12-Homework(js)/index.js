c04720a7ae04a7f3e626825f5e67f0069be1973d
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-10-29 14:12:49 -0700

    js commit

---
+++ 12-Homework(js)/index.js
@@ -0,0 +1,145 @@
+
+//ilk task 3 ededden en boyuk cem alinan kombinasiyani tapib yazmaq
+
+// let num1 = +prompt("enter a number:")
+// let num2 = +prompt("enter second number:")
+// let num3 = +prompt("enter third number:")
+// if(num1 > num2 && num2>num3){
+//     alert(`Big sum result is ${num1},${num2}`)
+// }
+// else if(num2 > num1 && num1>num3){
+//     alert(`Big sum result is ${num2},${num1}`)
+// }
+// else if(num2 > num1 && num3>num1){
+//     alert(`Big sum result is ${num2},${num3}`)
+// }
+// else if(num3 > num2 && num1>num2){
+//     alert(`Big sum result is ${num3},${num1}`)
+// }
+
+
+
+
+
+//ededlerin boyukden kiciye dogru duzulmesi 2 ci task
+
+// let a = +prompt("enter first number")
+// let b = +prompt("enter second number")
+// let c = +prompt("enter third number")
+
+
+// if(a>b && b>c){
+//     alert(`Big number${a}${b}${c}`)
+//     alert(`Small number${c}${b}${a}`)
+// }
+// else if(a>c && c>b){
+//     alert(`Big number${a}${c}${b}`)
+//     alert(`Small number${b}${c}${a}`)
+// }
+// else if(b>c && c>a){
+//     alert(`Big number${b}${c}${a}`)
+//     alert(`Small number${a}${c}${b}`)
+// }
+// else if(c>a && b>a){
+//     alert(`Big number${c}${b}${a}`)
+//     alert(`Small number${a}${b}${c}`)
+// }
+// else if(c>a && a>b){
+//     alert(`Big number${c}${a}${b}`)
+//     alert(`Small number${b}${a}${c}`)
+// }
+
+
+
+
+// duzbucaqli ucbucaq tapmaq 3 cu task
+
+
+// let a = +prompt("enter one side number")
+// let b = +prompt("enter other side number")
+// let c = +prompt("enter hypotenuse number")
+
+
+// if(a > 0 && b >0 && c >0){
+//     if(a**2 + b**2 === c**2){
+//         alert("bu duzbucaqli ucbucaqdir")
+//     }
+//     else{
+//         alert("bu duzbucaqli ucbucaq deyil yeke kisisen duz regem yaz")
+//     }
+// }
+// else{
+//     alert("musbet eded yaz")
+// }
+
+
+
+
+//verilen 3 reqemli ededi parcalamaq 4 cu task
+
+// let a = prompt("enter a number which at least 100")
+
+// if(a>=100){
+//     alert(`eded: ${a} netice: ${a[0]},${a[1]},${a[2]}`)
+// }
+// else{
+//     alert("3 reqemli eded yaz")
+// }
+
+
+
+
+
+
+
+
+//Aylarin adina gore nece gunden ibaret olmalari task 5
+
+
+// let a = prompt("enter month name")
+// a = a[0].toUpperCase()+a.slice(1);
+
+// if(a!=null){
+//     switch(a){
+//         case "January":
+//             alert("January has 31 days in it")
+//             break;
+//         case "Feburary":
+//                 alert("Feburary has 28 days in it")
+//                 break;
+//         case "March":
+//                 alert("March has 31 days in it")
+//                 break;
+//         case "April":
+//                 alert("April has 30 days in it")
+//                 break;
+//         case "May":
+//                 alert("May has 31 days in it")
+//                 break;
+//         case "June":
+//                 alert("June has 30 days in it")
+//                 break;
+//         case "July":
+//                 alert("July has 31 days in it")
+//                 break;
+//         case "Augost":
+//                 alert("Augost has 31 days in it")
+//                 break; 
+//         case "September":
+//                 alert("September has 30 days in it")
+//                 break;
+//         case "Oktober":
+//                 alert("Oktober has 31 days in it")
+//                 break;
+//         case "November":
+//                 alert("November has 30 days in it")
+//                 break;
+//         case "December":
+//                 alert("December has 31 days in it")
+//                 break;
+//         default:
+//             alert("bele bir ay yoxdur")
+//             break;
+//     }
+// }
+
