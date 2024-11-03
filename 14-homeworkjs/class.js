a65585d27631277d3e783c83d82b61d7160fe5e8
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-11-03 11:38:15 -0800

    commit

---
+++ 14-homeworkjs/class.js
@@ -0,0 +1,59 @@
+//#1
+
+// let number = prompt("eded daxil edin");
+// let number2 = prompt("quvvet ededini daxil edin");
+
+// function expression(number,number2) {
+//     if(number > 0){
+//         return number**number2;
+//     }
+// }
+// console.log(expression(number,number2))
+
+
+//#2
+
+
+// function expression(array,index,change) {
+//     let element = 0;
+//     for (let i = 0; i < array.length; i++) {
+//         if(array[i] === array[index]){
+//             element = array[change];
+//             array[change] = array[index];
+//             array[index] = element;
+//         }  
+//     }
+//     return array;
+// }
+// console.log(expression([10,20,30,40,50,60],4,2))
+
+//#3!
+
+// function expression(array) {
+//     let element;
+//     let newarray = [];
+//     for (let index = 0; index < array.length; index++) {
+//         element = array[index];
+//         for (let i = 1; i < array.length; i++) {
+//             if(array[i] != element){
+//                 newarray.push(array[i]);
+//             }  
+//         }
+//     }
+//     return newarray;
+// }
+// console.log(expression([10,20,30,40,50,60,50,60]))
+
+
+
+//#4!
+
+// function Capitalization(sentence) {
+//     for (let index = 0; index < sentence.length; index++) {
+//         if(sentence[index] === " "){
+            
+//         }
+//     }
+//     return sentence;
+// }
+// console.log(Capitalization("aslan memmedov"))
