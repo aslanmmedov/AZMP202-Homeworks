55fe127529d5a193a427b4c92a25a9d81d32f004
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-11-03 13:35:35 -0800

    commited

--- 14-homeworkjs/class.js
+++ 14-homeworkjs/class.js
@@ -27,20 +27,15 @@
 // }
 // console.log(expression([10,20,30,40,50,60],4,2))
 
-//#3!
+//#3-alinmadi
 
-// function expression(array) {
-//     let element;
+// function repeat(array) {
 //     let newarray = [];
 //     for (let index = 0; index < array.length; index++) {
-//         element = array[index];
 //         for (let i = 1; i < array.length; i++) {
-//             if(array[i] != element){
-//                 newarray.push(array[i]);
-//             }  
+            
 //         }
 //     }
-//     return newarray;
 // }
 // console.log(expression([10,20,30,40,50,60,50,60]))
 
@@ -49,11 +44,100 @@
 //#4!
 
 // function Capitalization(sentence) {
-//     for (let index = 0; index < sentence.length; index++) {
-//         if(sentence[index] === " "){
-            
-//         }
+//     let split = sentence.split(" ")
+//     for (let index = 0; index < split.length; index++) {
+//         split[index].capitalize();
 //     }
-//     return sentence;
+//     return split;
 // }
 // console.log(Capitalization("aslan memmedov"))
+
+
+//#5
+
+// function expression(array,array2) {
+//     for (let i = 0; i < array2.length; i++) {
+//         array.push(array2[i]) 
+//     }
+//     return array;
+// }
+// console.log(expression([10,20,30,40,50,60],[30,40,50,60]))
+
+
+//#6
+
+
+// function wordcounter(sentence) {
+//     let split = sentence.split(" ")
+//     let counter = 0;
+//     for (let index = 0; index < split.length-1; index++) {
+//         counter++;
+//     }
+//     return counter;
+// }
+// console.log(wordcounter("aslan memmedov aslan aslan aslqn aslan aslan aslan aslan"))
+
+
+
+//#7
+
+// function median(array) {
+//     let median;
+//     for (let index = 0; index < array.length; index++) {
+//         if(index+1 === array.length/2){
+//             median = (array[index]+array[index+1])/2;
+//         }
+//         if(array.length %2 != 0){
+//             median = array[(array.length/2) -0.5];
+//         }
+//     }
+//     return median;
+// }
+// console.log(median([10,20,30,40,50,60,70,80,90]))
+
+
+
+//#8
+
+
+
+
+
+//#9
+
+// function word(wordd,letter) {
+//     let cem = 0;
+//     counter = 0;
+//     for (let index = 0; index < wordd.length; index++) {
+//         if(wordd[index] === letter){
+//             cem+=index;
+//             counter++;
+//         }
+//     }
+//     if(counter === 0){
+//         return -1;
+//     }
+//     return cem;
+// }
+
+// console.log(word("aslan","a"))
+
+
+//#10
+
+
+
+// function countries(names) {
+//     let names2 = [];
+//     let name = "";
+//     for (let index = 0; index < names.length; index++) {
+//         for (let i = 0; i < 3; i++) {
+//             if(i != 3){
+                
+//             }
+//         }
+//     }
+//     return name;
+// }
+
+// console.log(name)
