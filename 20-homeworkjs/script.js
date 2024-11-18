3ca3c7306cb02b5a80d645c3602ad0a90bbddf15
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-11-18 01:50:10 -0800

    commit

--- 20-homeworkjs/script.js
+++ 20-homeworkjs/script.js
@@ -6,23 +6,42 @@ let allpend = document.querySelector(".all-pend")
 let parag = document.querySelector(".parag");
 let spn = document.querySelector(".num")
 let todos = document.querySelector(".todos");
+let idcount = 0;
+   
 add.addEventListener("click",function(){
+    idcount++;
+    spn.textContent = idcount;
     let list = document.createElement("div");
-
+    let idlist = list.id = idcount;
     list.innerHTML = `
     <div class="todos-p">
+                <div class ="row1">
                 <input type="checkbox" class = "check">
                 <h4 class="h4">${inpt.value}</h4>
+                </div>
+                <div><button class="edit">Edit</button>
+            <button class="delete">Delete</button></div>
+        </div>
             </div>
-    `
-    todos.appendChild(list);
+    `;
     let check = document.querySelector(".check");
     let h4 = document.querySelector(".h4")
-    check.addEventListener("check",function(){
+    let edit = document.querySelector(".edit");
+    let dlt = document.querySelector(".delete")
+    todos.appendChild(list);
+
+    check.addEventListener("click",function(){
         h4.innerHTML = `
         <del class="h4">${inpt.value}</del>
         `
     });
+    
+    edit.addEventListener("click",function(){
+        
+        inpt.textContent = "aaaaaaa";
+    })
 })
+
+
 
 
