1994de0b085dc2081dca4ef1516dcbf01e480e8d
aslanmmedov <aslanzm-azmp202@code.edu.az>
2024-12-24 23:48:00 -0800

    commit

---
+++ 26-HomeworkRoute/Reactrouterdoom/src/layout/header/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Link } from "react-router";
+
+const Header = () => {
+  return (
+    <div>
+        <h2>Logo</h2>
+        <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+        </ul>
+        <div className="btns">
+          <button><Link to="/login">Login</Link></button>
+          <button><Link to="/register">Register</Link></button>
+        </div>
+    </div>
+  )
+}
+
+export default Header
